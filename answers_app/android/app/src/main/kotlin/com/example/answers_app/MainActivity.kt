70a9cd3d3f8360bb872fbf75a13e38f0ac2b2369
adeuSH1 <112491144+adeuSH1@users.noreply.github.com>
2024-08-01 17:47:06 +0300

    first one

---
+++ answers_app/android/app/src/main/kotlin/com/example/answers_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.answers_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ answers_app/lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 87, 148, 179),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 188, 8, 128),
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
